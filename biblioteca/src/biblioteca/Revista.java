296652c37839311f74cf733f64d2374bd2aa36d3
Tiago <tiagoelr@hotmail.com>
2020-03-30 14:39:06 -0300

    Criação da classe Revista

---
+++ biblioteca/src/biblioteca/Revista.java
@@ -0,0 +1,22 @@
+package biblioteca;
+
+public class Revista {
+	
+	private String nome;
+	private String edicao;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEdicao() {
+		return edicao;
+	}
+	public void setEdicao(String edicao) {
+		this.edicao = edicao;
+	}
+	
+	
+
+}
