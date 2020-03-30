c2bc824b383af1dd68afec1e7e83b573336d2f3d
Tiago <tiagoelr@hotmail.com>
2020-03-30 14:03:18 -0300

    Criação da classe Livro

---
+++ biblioteca/src/biblioteca/Livro.java
@@ -0,0 +1,16 @@
+package biblioteca;
+
+public class Livro {
+	
+	private String title;
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	
+}
