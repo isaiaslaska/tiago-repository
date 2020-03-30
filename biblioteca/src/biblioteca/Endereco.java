4937b3c849fccbff77af4c050b8365c3b1e91115
Tiago <tiagoelr@hotmail.com>
2020-03-30 15:00:56 -0300

    Criação da classe Endereço

---
+++ biblioteca/src/biblioteca/Endereco.java
@@ -0,0 +1,21 @@
+package biblioteca;
+
+public class Endereco {
+
+	private String rua;
+	private String numero;
+	public String getRua() {
+		return rua;
+	}
+	public void setRua(String rua) {
+		this.rua = rua;
+	}
+	public String getNumero() {
+		return numero;
+	}
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+	
+	
+}
